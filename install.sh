a65c35acfc0ac7ba42e85ece736a45980a2399cd
Portuguez Padrón Angel <165244123+Fimbrostilk@users.noreply.github.com>
2026-05-16 18:14:46 -0600

    Remove git clone command from install script

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-git clone https://github.com/Fimbrostilk/accel-on-reports
 cd accel-on-reports
 mkdir /usr/share/texmf/tex/latex/esime-report/
 sudo cp accel-on-reports/templates/esime-report.cls /usr/share/texmf/tex/latex/esime-report/
