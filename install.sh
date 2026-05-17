dc2445332bf8b7a9d84dde33ea3bb51c3993afc0
Portuguez Padrón Angel <165244123+Fimbrostilk@users.noreply.github.com>
2026-05-16 18:18:02 -0600

    Fix paths for copying template files in install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 mkdir /usr/share/texmf/tex/latex/esime-report/
-sudo cp accel-on-reports/templates/esime-report.cls /usr/share/texmf/tex/latex/esime-report/
-sudo cp -r accel-on-reports/templates/Imagenes/* /usr/share/texmf/tex/latex/esime-report/
+sudo cp templates/esime-report.cls /usr/share/texmf/tex/latex/esime-report/
+sudo cp -r templates/Imagenes/* /usr/share/texmf/tex/latex/esime-report/
