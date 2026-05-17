a34f255db65149cacf5fc57dda0578954c9f4bf7
Portuguez Padrón Angel <165244123+Fimbrostilk@users.noreply.github.com>
2026-05-16 18:15:59 -0600

    Remove directory change from install script

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-cd accel-on-reports
 mkdir /usr/share/texmf/tex/latex/esime-report/
 sudo cp accel-on-reports/templates/esime-report.cls /usr/share/texmf/tex/latex/esime-report/
 sudo cp -r accel-on-reports/templates/Imagenes/* /usr/share/texmf/tex/latex/esime-report/
