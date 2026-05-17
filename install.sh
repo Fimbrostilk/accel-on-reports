26fdd8f0ba7b948a4ec039bcf60e2d085742de0f
Portuguez Padrón Angel <165244123+Fimbrostilk@users.noreply.github.com>
2026-05-16 18:22:19 -0600

    Fix image directory path in install script

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 
 mkdir /usr/share/texmf/tex/latex/esime-report/
 sudo cp templates/esime-report.cls /usr/share/texmf/tex/latex/esime-report/
-sudo cp -r templates/imagenes/* /usr/share/texmf/tex/latex/esime-report/
+sudo cp -r templates/imagenes/* /usr/share/texmf/tex/latex/esime-report/imagenes/
 sudo texhash
