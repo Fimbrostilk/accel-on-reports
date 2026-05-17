f65e1dfb6ac094cfc2f12cba4b8d03492f3b63f7
Portuguez Padrón Angel <165244123+Fimbrostilk@users.noreply.github.com>
2026-05-16 18:19:23 -0600

    Fix case sensitivity in image directory copy command

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@
 
 mkdir /usr/share/texmf/tex/latex/esime-report/
 sudo cp templates/esime-report.cls /usr/share/texmf/tex/latex/esime-report/
-sudo cp -r templates/Imagenes/* /usr/share/texmf/tex/latex/esime-report/
+sudo cp -r templates/imagenes/* /usr/share/texmf/tex/latex/esime-report/
